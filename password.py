a70d50748ee947ef1f3891c07aa0d957bace4f7b
Ngoc D <dominhngoc.267@gmail.com>
2024-01-06 12:25:29 +0700

    2nd draft

--- password.py
+++ password.py
@@ -6,22 +6,31 @@ def pw_validation(passwd):
         print('Password must be at least 8 characters')
     elif not re.search('[A-Z]', passwd):
         print('Password must contain at least 1 uppercase letter')
-    elif not re.search('[0-9]', passwd):
-        print('Password must contain at least 1 number')
     elif not re.search(r'\W', passwd):
         print('Password must contain at least 1 special character')
-    elif re.search(r'^\d{3}$', passwd):
-        print('Password must not contain 3 consecutive numbers')
+    elif not re.search('[0-9]', passwd):
+        print('Password must contain at least 1 number')
     else:
+        d = re.findall('[0-9]+', passwd)
+        for i in range(len(d)+1):
+            if len(d[i]) >= 3:
+                for j in range(len(d[i])-1):
+                    if (int(d[i][j]) == int(d[i][j+1]) - 1) and (int(d[i][j+1]) == int(d[i][j+2]) - 1):
+                        print('Password must not contain 3 consecutive numbers')
+                    else:
+                        break
         print('Password is valid')
-        return False
+
+
+# re.search(r'^\d{3}$', passwd)
 
 
 while True:
     pw = input('Your password: ')
-    if not pw_validation(pw) is False:
-        continue
+    if not pw_validation(pw):
+        pass
     else:
         break
 
-
+# a = '33455@@F'
+# pw_validation(a)
